6de36304fc745004f45b7923312bb33d1d462f42
Manuel Francisco Naranjo <manuel@aircable.net>
2011-11-05 23:17:38 -0700

    Added build script for android

--- android.sh
+++ android.sh
@@ -2,6 +2,15 @@
 
 rm -rf android
 mkdir android
+
+make clean
+./configure --host=arm-linux-androideabi \
+    --with-uart-device=/dev/ttyHS0 \
+    --enable-powermanagement \
+    --disable-bluetool \
+    --enable-android
+make
+
 cp src/BTdaemon android
 cp src/lib*.so android
 cp $(find example -maxdepth 1 -type f -executable) android
